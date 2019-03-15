02e8ebc90d4ec3518f93f4c1c9715ae52333634c
Daymos1996 <38887719+Daymos1996@users.noreply.github.com>
2019-03-15 14:02:19 +0100

    Rejestracja wstepna

--- app/src/main/java/login/LoginActivity.java
+++ app/src/main/java/login/LoginActivity.java
@@ -1,17 +1,50 @@
 package login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import daymos.lodz.uni.math.pl.mobilefleet.R;
+import register.ChoiceRegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView registerTextView;
+    private Button buttunLogIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        init();
+
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ChoiceRegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+    private void init() {
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        buttunLogIn = findViewById(R.id.buttunLogIn);
+        registerTextView = findViewById(R.id.textViewRegister);
+
+    }
+
 }
---
+++ app/src/main/java/register/ChoiceRegisterActivity.java
@@ -0,0 +1,15 @@
+package register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import daymos.lodz.uni.math.pl.mobilefleet.R;
+
+public class ChoiceRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_register);
+    }
+}
