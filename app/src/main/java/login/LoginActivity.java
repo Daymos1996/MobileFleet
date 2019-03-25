8c7ee2c5b73affbe53e7d917eec511ebfe215286
Daymos1996 <38887719+Daymos1996@users.noreply.github.com>
2019-03-25 20:33:47 +0100

    Login success

--- app/src/main/java/login/LoginActivity.java
+++ app/src/main/java/login/LoginActivity.java
@@ -1,6 +1,8 @@
 package login;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,14 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import daymos.lodz.uni.math.pl.mobilefleet.R;
 import register.ChoiceRegisterActivity;
@@ -18,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private TextView registerTextView;
     private Button buttunLogIn;
+
+    private FirebaseDatabase database;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+
 
 
     @Override
@@ -35,15 +50,49 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, ChoiceRegisterActivity.class);
             startActivity(intent);
 
-        }
-    });
+            }
+        });
+        buttunLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+
+
+            }
+        });
 }
 
     private void init() {
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
         emailEditText = findViewById(R.id.editTextEmail);
         passwordEditText = findViewById(R.id.editTextPassword);
         buttunLogIn = findViewById(R.id.buttunLogIn);
         registerTextView = findViewById(R.id.textViewRegister);
+
+    }
+
+    private void LoginUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            user = auth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this,"Udalo sie zalogowac",Toast.LENGTH_LONG).show();
+
+                            //Intent intent = new Intent(LoginActivity.this, ProfilActivity.class);
+                           // startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Błąd logowania", Toast.LENGTH_LONG).show();
+                           }
+                    }
+                });
 
     }
 
